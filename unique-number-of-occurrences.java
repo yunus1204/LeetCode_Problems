3b29e89b59bbdfde7645c7a185367d15c041cb19
Mohammed Yunus <142818306+yunus1204@users.noreply.github.com>
2024-01-17 22:04:06 +0530

    Create unique-number-of-occurrences.java

---
+++ unique-number-of-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:arr)
+        {
+           map.put(i,map.getOrDefault(i,0)+1);
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:map.values())
+        {
+            if(!set.add(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
