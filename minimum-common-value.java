5f78896795fb943972d79ec99203351986116d86
Mohammed Yunus <142818306+yunus1204@users.noreply.github.com>
2024-03-10 00:03:35 +0530

    Create minimum-common-value.java

---
+++ minimum-common-value.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+           
+
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+
+            if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return -1;
+    }
+}
